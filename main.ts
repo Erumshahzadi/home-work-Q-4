f2bf55460ead4d07ef7d78d2d9fba9fd3779916d
Erum shahzadi <163527258+Erumshahzadi@users.noreply.github.com>
2024-04-08 11:44:42 +0500

    vacation  work by Sir Hamza (9 to 12 am)

---
+++ main.ts
@@ -0,0 +1,23 @@
+// Question4: Create a program to check a student grade and perchentage.
+const per:number = 60;
+if(per <= 34){
+    console.log("Your are failed");
+}
+if(per >=35  && per <=45){
+    console.log("Your grade is E");
+}
+if(per >=46  && per <=50 ){
+    console.log("Your grade is D");
+}
+else if(per >=51  && per <=55 ){
+    console.log("Your grade is C");
+}
+if(per >=60  && per <=68 ){
+    console.log("Your grade is B");
+}
+if(per >=70  && per <=75 ){
+    console.log("Your grade is A");
+}
+if(per >=80 ){
+    console.log("Your grade is A+");
+}
